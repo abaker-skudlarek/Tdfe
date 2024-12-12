ce0d0ca9b4d14c3171071af291c536b4c394edd1
abaker-skudlarek <abakerskudlarek@gmail.com>
2024-12-12 00:22:52 -0800

    file_selector scene finished

---
+++ Scenes/FileSelector/file_selector.gd
@@ -0,0 +1,32 @@
+extends VBoxContainer
+
+@onready var option_button: OptionButton = $FileBrowser/OptionButton
+
+
+func get_files_in_dir(dir_path: String) -> Array:
+	var dir := DirAccess.open(dir_path)
+	var files_found := []
+	if dir:
+		dir.list_dir_begin()
+		var file_name := dir.get_next()
+		while file_name != "":
+			if !dir.current_is_dir() and file_name.get_extension() == "yaml":
+				files_found.append(file_name)
+			file_name = dir.get_next()
+	else:
+		printerr("An error ocurred when trying to acces the path " + dir_path)
+
+	return files_found
+
+
+func _on_browse_button_pressed() -> void:
+	$FolderBrowserWindow.visible = true
+
+
+func _on_folder_browser_window_dir_selected(dir: String) -> void:
+	$FolderBrowser/LineEdit.text = dir
+	var files: Array = get_files_in_dir(dir)
+	if option_button.has_selectable_items():
+		option_button.clear()
+	for file: String in files:
+		option_button.add_item(file)
